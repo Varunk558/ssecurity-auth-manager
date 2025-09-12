f36bbee6792cc5a5772a03ee2d001fe79dcd869b
Varun <varunkothapally18@gmail.com>
2025-09-12 09:55:34 -0500

    Created User entity

---
+++ src/main/java/com/ssam/entity/User.java
@@ -0,0 +1,20 @@
+package com.ssam.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String username;
+
+    private String password;
+}
