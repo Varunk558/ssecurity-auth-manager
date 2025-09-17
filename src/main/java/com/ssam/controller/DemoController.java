e16296c68c616ca1f8f5b0aa2f5ac0f3f909a065
Varun <varunkothapally18@gmail.com>
2025-09-17 13:36:21 -0500

    Created a DemoController

---
+++ src/main/java/com/ssam/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.ssam.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/demo")
+    public String demo(){
+
+        var u = SecurityContextHolder.getContext().getAuthentication();
+        u.getAuthorities().forEach(System.out::println);
+        return "Demo";
+    }
+}
