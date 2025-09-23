aee3f953e944c2e1c14b2bef72494d93b7be5abd
Varun <varunkothapally18@gmail.com>
2025-09-22 19:52:42 -0500

    Added SecurityAuthority class

---
+++ src/main/java/com/ssam/security/SecurityAuthority.java
@@ -0,0 +1,16 @@
+package com.ssam.security;
+
+import com.ssam.entity.Authority;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+@AllArgsConstructor
+public class SecurityAuthority implements GrantedAuthority {
+
+    private final Authority authority;
+
+    @Override
+    public String getAuthority() {
+        return authority.getName();
+    }
+}
