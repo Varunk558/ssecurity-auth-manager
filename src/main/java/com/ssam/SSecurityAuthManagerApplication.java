f7c6bf3bfa2424321143fdad89bfd4619194f580
Varun <varunkothapally18@gmail.com>
2025-09-22 19:53:48 -0500

    Added main method class

---
+++ src/main/java/com/ssam/SSecurityAuthManagerApplication.java
@@ -0,0 +1,16 @@
+package com.ssam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+
+@SpringBootApplication
+public class SSecurityAuthManagerApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(SSecurityAuthManagerApplication.class, args);
+    }
+}
